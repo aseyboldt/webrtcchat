227275551e62dc4fb3c9dbf606e7674302c6250d
Adrian Seyboldt <adrian.seyboldt@gmail.com>
2013-08-14 18:24:07 +0200

    init

---
+++ main.js
@@ -0,0 +1,42 @@
+// markdown = require('markdown')
+marked = require('marked')
+codemirror = require('codemirror')
+hljs = require('highlight.js')
+
+marked.setOptions({
+    highlight: function(code, lang) {
+        if (hljs.LANGUAGES[lang]) {
+            console.log(hljs.highlightAuto(code).value);
+            return hljs.highlight(lang, code).value;
+        }
+        else {
+            console.log(hljs.highlightAuto(code).value);
+            return hljs.highlightAuto(code).value;
+        }
+    }
+});
+
+function sendMessage(message) {
+    console.log(message);
+};
+
+var editor = CodeMirror.fromTextArea(document.getElementById("text-input"), {
+    mode: 'markdown',
+    lineNumbers: true,
+    theme: "default"
+});
+
+
+editor.on("change", function (cm, changeObj) {
+    var preview = document.getElementById("preview");
+    preview.innerHTML = marked(editor.getValue());
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub, "preview"]);
+});
+
+
+editor.on("keyup", function (cm, event) {
+    if (event.which == 13 && event.shiftKey) {
+        sendMessage(editor.getValue());
+        editor.setValue("");
+    }
+});
